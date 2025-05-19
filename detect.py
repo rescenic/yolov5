8d8d35efbdd0e5052dc1d54c2a7b19f91c0178cf
Muhammad Ridwan Hakim, S.T., CPITA, ITPMCP <adminrescen@gmail.com>
2025-05-19 15:37:59 +0700

    Windows Env

--- detect.py
+++ detect.py
@@ -36,6 +36,12 @@ import platform
 import sys
 from pathlib import Path
 
+
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+
 import torch
 
 FILE = Path(__file__).resolve()
